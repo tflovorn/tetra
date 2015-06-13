169cfd081b4ddaee8a2e71869d204c4b0d462a2e
Tim Lovorn <tflovorn@crimson.ua.edu>
2015-06-13 17:52:50 -0500

    interface for integration weights

--- tetra/weights.py
+++ tetra/weights.py
@@ -1,7 +1,12 @@
-def DosBands(E, submesh, tetras, Eks):
-    '''Return a list of the contributions to the density of states at energy E
-    from each band index, summed over all tetrahedra.
-    The calculation of D_T(E) is implemented as described in BJA94 Appendix C.
+def Weights(E_Fermi, submesh, tetras, Eks):
+    '''Return a list in which each element is a list of integration weights.
+    The first index for the returned list specifies a band index, and the
+    second list specifies a k-point index; i.e. the returned list
+    w[n][j] = w_{nj}.
+    The calculation of w_{nj} is implemented as described in BJA94 Appendix B
+    and Section V.
+
+    E_Fermi = Fermi energy of the system.
 
     submesh = a list of k-points covering the Brillouin zone, determined as
     described in BJA94 Section III.
@@ -13,18 +18,23 @@ def DosBands(E, submesh, tetras, Eks):
 
     Eks = a list in which each element is a sorted list of eigenstate energies
     E_n(k), with k being the k-point at the corresponding element of submesh.
+
+    bandIndex = the band index n to consider, corresponding to n in E_n(k).
     '''
     pass
 
-def DosContrib(E, submesh, tetra, Eks, bandIndex):
-    '''Return the contribution to the density of states at energy E from the
-    specified band index and tetrahedron.
-    The calculation of D_T(E) is implemented as described in BJA94 Appendix C.
+def WeightContrib(E_Fermi, submesh, tetra, Eks, kN, bandIndex):
+    '''Return the specified tetrahedron's contribution to the integration
+    weight w_{bandIndex, kN}.
+    The calculation of w_{nj} is implemented as described in BJA94 Appendix B
+    and Section V.
+
+    E_Fermi = Fermi energy of the system.
 
     submesh = a list of k-points covering the Brillouin zone, determined as
     described in BJA94 Section III.
 
-    tetras = a tuple of the form (kN1, kN2, kN3, kN4) denoting the
+    tetra = a tuple of the form (kN1, kN2, kN3, kN4) denoting the
     vertices of a tetrahedron to include in the Brillouin zone summation,
     where the kN's are indices of submesh (i.e. submesh[kN1] = k1, etc.).
     The tetrahedra must be constructed as described in BJA94 Section III.
@@ -34,4 +44,6 @@ def DosContrib(E, submesh, tetra, Eks, bandIndex):
 
     bandIndex = the band index n to consider, corresponding to n in E_n(k).
     '''
+    if kN not in tetra:
+        return 0.0
     pass
